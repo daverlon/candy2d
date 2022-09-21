3278682f194e8f0e89f478551b634973c1ebf9bf
candyslug <david.wip.long@gmail.com>
2022-09-21 21:20:42 +0800

    fixed memory leak

--- src/main.cpp
+++ src/main.cpp
@@ -289,6 +289,8 @@ int main() {
         // debug
         SDL_SetWindowTitle(g::window, (std::to_string(g::time.DeltaTime()) + ", " + std::to_string(g::time.FPS())).c_str());
 
+        SDL_DestroyTexture(viewport_texture);
+
         // update delta time
         g::time.UpdateLast(SDL_GetTicks());
     }
